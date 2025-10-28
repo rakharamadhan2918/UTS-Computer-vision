b56d82ddf1e4128539f869cf6335965aa2021d05
rakharamadhan2918 <rakharamadhan2918@gmail.com>
2025-10-28 22:19:44 +0700

    Add files via upload

---
+++ UTS computer vision Rakha/karakter.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import os
+
+ 
+os.makedirs("output", exist_ok=True)
+canvas = np.full((400, 400, 3), 255, dtype=np.uint8)
+
+cv2.rectangle(canvas, (140, 180), (260, 320), (150, 100, 50), -1)  
+pts = np.array([[200, 100], [130, 180], [270, 180]], np.int32)
+cv2.fillPoly(canvas, [pts], (100, 60, 30))  
+cv2.circle(canvas, (200, 230), 25, (0, 0, 0), -1)
+cv2.rectangle(canvas, (190, 320), (210, 370), (80, 50, 20), -1)
+cv2.line(canvas, (180, 260), (220, 260), (0, 0, 0), 4)
+
+cv2.imwrite("output/rumah_burung.png", canvas)
+print("Gambar rumah burung disimpan di folder output/rumah_burung.png")
